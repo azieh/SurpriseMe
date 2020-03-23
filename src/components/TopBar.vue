5a1fd7f30cdbcbbb6e9a1ba5d0aa77e89a52c944
Adrian Wasielewski <adrian.wasielewski@pl.abb.com>
2020-03-23 21:36:13 +0100

    First step

---
+++ src/components/TopBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app color="white" height="100">
+      <v-img
+        contain
+        max-height="70%"
+        src="../img/surpriseme_color_bn.png"
+      ></v-img>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "TopBar"
+};
+</script>
