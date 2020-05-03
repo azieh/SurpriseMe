6bd9d1638e4c8669a8867bf7f27a1f36896b8aaf
Adrian Wasielewski <aziehx@o2.pl>
2020-05-03 14:33:35 +0200

    Add google analytics 2

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import Meta from 'vue-meta';
-import VueAnalytics from 'vue-analytics';
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "UA-165404331-1" }
+});
 Vue.use(Meta);
 Vue.use(Vuetify);
-Vue.use(VueAnalytics, {
-    id: 'UA-165404331-1'
-  });
 
 export default new Vuetify({
 });
