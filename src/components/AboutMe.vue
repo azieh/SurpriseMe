6e4d8eac8c763b706188921f475dae961ac8f81f
Adrian Wasielewski <aziehx@o2.pl>
2020-05-03 11:22:03 +0200

    Merge changes

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,7 @@
     <div class="py-12"></div>
 
     <v-container class="text-center">
-      <h2 class="display-2 font-weight-bold mb-3">Kim jestem?</h2>
+      <h2 class="display-2 font-weight-bold mb-3">Surprise ME to ja Martyna</h2>
 
       <v-responsive class="mx-auto mb-8" width="56">
         <v-divider class="mb-1"></v-divider>
@@ -11,37 +11,53 @@
         <v-divider></v-divider>
       </v-responsive>
 
-      <v-responsive
-        class="mx-auto title font-weight-light mb-8"
-        max-width="720"
-      >Surprise ME to ja Martyna. Miło Cie poznać! Właśnie zaczynasz podróż do mojego skrawka Internetu. Miejsca, gdzie każda rzecz została stworzona własnoręcznie, a ceny odzwierciedlają czas pracy spędzony podczas nadawaniu jej kształtu. Tworząc markę postanowiłam zwracać szczególną uwagę właśnie na czas. Zanim zaczęłam szyć nie zdawałam sobie sprawy, ile pracy należy włożyć w wykonanie jednej rzeczy, która wisi u mnie w szafie. Właśnie z tego powodu, postanowiłam nie tylko sprzedawać wspaniałe rzeczy, ale i szczyptę wiedzy o procesie produkcji w moim domu. Nazwa każdego produktu to ilość godzin spędzonych podczas przygotowywania go dla Ciebie. Surprise ME to ubrania i akcesoria na miarę naszych czasów. Każde zaprojektowane tak, by dobrze wyglądało w różnych sytuacjach. To kanapowe ubrania, w których można wyjść na jogę czy skoczyć w góry. Surprise ME powstało z zamiłowania do etycznej mody, leżenia na kanapie i pracy własnych rąk.</v-responsive>
+      <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
+        Miło Cię poznać!
+        Właśnie zaczynasz podróż do mojego skrawka Internetu.
+        Miejsca, gdzie każda rzecz została stworzona własnoręcznie, a ceny odzwierciedlają czas pracy spędzony podczas nadawaniu jej kształtu.
+        Tworząc markę, postanowiłam, zwracać szczególną uwagę właśnie na czas. Zanim zaczęłam szyć, nie zdawałam sobie sprawy, ile pracy należy włożyć w wykonanie jednej rzeczy, która wisi u mnie w szafie. Właśnie z tego powodu, postanowiłam nie tylko sprzedawać wspaniałe rzeczy, ale i szczyptę wiedzy o procesie produkcji w moim domu. Nazwa każdego produktu to ilość godzin spędzonych podczas przygotowywania go dla Ciebie.
+        Surprise ME to ubrania i akcesoria na miarę naszych czasów. Każde zaprojektowane tak, by dobrze wyglądało w różnych sytuacjach. To kanapowe ubrania, w których można wyjść na jogę czy skoczyć w góry. Surprise ME powstało z zamiłowania do etycznej mody, leżenia na kanapie i pracy własnych rąk.
+      </v-responsive>
 
       <v-avatar class="elevation-12 mb-12" size="128">
         <v-img src="../img/portret.jpg"></v-img>
       </v-avatar>
       <div></div>
-      
-      <h2 class="display-2 font-weight-bold mb-3">Bądź ze mną na bierząco</h2>
 
+      <h2 class="display-2 font-weight-bold mb-3">Odwiedź mnie</h2>
+      <v-responsive class="mx-auto mb-12" width="56">
+        <v-divider class="mb-1"></v-divider>
+        <v-divider></v-divider>
+      </v-responsive>
       <v-row no-gutters>
         <v-col class="text-right">
-           <a href="https://www.facebook.com/surprisemeatelier/">
-          <v-avatar class="elevation-12 mb-12" size="200">
-            <v-img src="../img/facebook.png"></v-img>
-          </v-avatar>
-           </a>
+          <a href="https://www.facebook.com/surprisemeatelier/">
+            <v-avatar class="elevation-12 mb-12" size="200">
+              <v-img src="../img/facebook.png"></v-img>
+            </v-avatar>
+          </a>
         </v-col>
-        
-        <v-col class="text-left">
+        <v-col class="text-center">
           <a href="https://www.instagram.com/surprisemeatelier/">
-          <v-avatar class="elevation-12 mb-12" size="200">
-            <v-img src="../img/insta.png"></v-img>
-          </v-avatar>
+            <v-avatar class="elevation-12 mb-12" size="200">
+              <v-img src="../img/insta.png"></v-img>
+            </v-avatar>
+          </a>
+        </v-col>
+        <v-col class="text-left">
+          <a href="mailto:hello.surpriseme@gmail.com">
+            <v-avatar color="teal" class="elevation-12 mb-12" size="200">
+              <v-img src="../img/at.png"></v-img>
+            </v-avatar>
           </a>
         </v-col>
       </v-row>
+       <v-responsive class="mx-auto mb-12" width="56">
+        <v-divider class="mb-1"></v-divider>
+        <v-divider></v-divider>
+      </v-responsive>
       <div class="py-12"></div>
-      <h2 class="display-2 font-weight-bold mb-3">Zamówienia możesz składać na</h2>
+      <h2 class="display-2 font-weight-bold mb-3">Moje bluzy i akcesoria znajdziesz na</h2>
     </v-container>
   </section>
 </template>
